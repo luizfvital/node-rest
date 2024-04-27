de04999ce9c4de25207174bc2d67eb8780a603df
Luiz Fernando Vital <luizfvital@gmail.com>
2024-04-26 21:14:58 -0300

    add eslint

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import fastify from 'fastify';
+import fastify from 'fastify'
 
-const app = fastify();
+const app = fastify()
 
-app.get('/', () => { });
+// app.get('/', () => { })
 
-app.listen({
-  port: 3333
-}).then(() => console.log('listening on port 3333...'));
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => console.log('listening on port 3333...'))
