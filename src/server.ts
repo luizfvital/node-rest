6b58b86643efaf76358a7e737e0e44ed40cad5f8
Luiz Fernando Vital <luizfvital@gmail.com>
2024-04-29 14:54:08 -0300

    add knex and create `database.ts`

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
 
-// app.get('/', () => { })
+app.get('/', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+
+  return tables
+})
 
 app
   .listen({
